94a496e9ac82645021220a857bab81251c1b2656
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2023-04-18 22:58:55 +0300

    Create scripts to create DB , init DB data, Required request (#4)

---
+++ Db_create.sql
@@ -0,0 +1,36 @@
+﻿USE [master]
+GO
+
+if db_id('ProductCategoryDB') is null
+	BEGIN 
+		CREATE DATABASE ProductCategoryDB;
+	END;
+ELSE
+	BEGIN
+	 ALTER DATABASE ProductCategoryDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	 DROP DATABASE ProductCategoryDB
+	 CREATE DATABASE ProductCategoryDB;
+	END
+GO
+
+USE ProductCategoryDB
+GO
+
+
+CREATE TABLE Products (id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                          name VARCHAR(100) NOT NULL,
+)
+
+CREATE TABLE Category (id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                          name VARCHAR(100) NOT NULL,
+)
+
+CREATE TABLE Products_Category (
+                                   product_id INT NOT NULL,
+                                   category_id INT NOT NULL,
+                                   PRIMARY KEY (product_id, category_id),
+                                   FOREIGN KEY (product_id)
+                                       REFERENCES Products (id) ON DELETE CASCADE,
+                                   FOREIGN KEY (category_id)
+                                       REFERENCES Category (id) ON DELETE CASCADE
+)
---
+++ Db_init.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO Products values ('notebook'), ('cheese'), ('smth'), ('bicycle'),('car');
+INSERT INTO Category values ('Cat1'), ('Cat2');
+INSERT INTO Products_Category values (1, 2), (2, 1), (2, 2), (4, 2),(5,2);
