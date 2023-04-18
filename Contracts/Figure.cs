b44c63b1ca05ed033f3fb96407350262fdbf8a77
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2023-04-18 19:58:41 +0300

    Create basic figures Implement calc area methods Add tests

---
+++ Contracts/Figure.cs
@@ -0,0 +1,11 @@
+﻿namespace FigureLib.Contracts
+{
+    public abstract class Figure
+    {
+        /// <summary>
+        /// Calculate area of figure
+        /// </summary>
+        /// <returns></returns>
+        public abstract double CalcArea();
+    }
+}
---
+++ Figures/Circle.cs
@@ -0,0 +1,30 @@
+﻿using FigureLib.Contracts;
+
+namespace FigureLib.Figures
+{
+    public sealed class Circle : Figure
+    {
+        private readonly double _radius;
+
+        /// <summary>
+        /// Create circle with scpecified radius
+        /// </summary>
+        /// <param name="radius">Positive number represents radius of circle</param>
+        /// <exception cref="ArgumentOutOfRangeException">Throw if number less or eqaual zero</exception>
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius), "Circle radius must be a positive number");
+            }
+
+            _radius = radius;
+        }
+
+
+        public override double CalcArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+}
