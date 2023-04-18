7cc3de22f7fe14529b1a8fc8b28d72f756a8fb00
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2023-04-18 21:47:59 +0300

    Add test to lib

---
+++ FigureLib.Tests/CircleTests.cs
@@ -0,0 +1,23 @@
+using FigureLib.Figures;
+using Xunit;
+
+namespace FigureLib.Tests
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void InputValidationTest()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(0));
+        }
+
+        [Fact]
+        public void AreaCalcTest()
+        {
+            Circle circle = new(3);
+
+            Assert.Equal(28.274, circle.CalcArea(), 3);
+        }
+    }
+}
